6bc064c1a5750deb78dc2ba91bff6dda05401d04
jgore077 <jgore077@gmail.com>
2023-09-02 12:09:54 -0400

    modified:   papers.py

--- papers.py
+++ papers.py
@@ -7,7 +7,7 @@ PAGES=10
 START=10000
 MAX_PAGES=2748
 
-
+# Simple class for storing basic info
 class Paper():
     def __init__(self,title:str,authors:str,abstract:str,keywords:str):
         self.title=title
@@ -24,34 +24,41 @@ def turn_into_tsv_row(paper:Paper):
 def read_tsv_and_label(input_file):
     labeled_data = []
     
-    with open(input_file, "r",encoding='UTF-8', newline='') as tsv_file:
+    with open(input_file, "r",encoding='UTF-8') as tsv_file:
         for line in tsv_file:
             fields = line.strip().split("\t")
-            if len(fields) == 4:
-                labeled_row = {
-                    "Title": fields[0],
-                    "Authors": fields[1],
-                    "Abstract": fields[2],
-                    "Keywords": fields[3]
-                }
-                labeled_data.append(labeled_row)
+            labeled_row = {
+                "Title": fields[0],
+                "Authors": fields[1],
+                "Abstract": fields[2],
+                "Keywords": fields[3]
+            }
+            labeled_data.append(labeled_row)
     return labeled_data
 
 
-
+# Opening tsv file
 with open("papers.tsv","w",encoding='UTF-8', newline='') as tsv:
-
-    for page_num in range(0,MAX_PAGES+1):
+    # Creating a loop to generated the number found in the PATH_TO_PAPERS dir
+    for page_num in range(0,PAGES+1):
+        
         paper = Paper('None','None','None','None')
-
+        
+        # Opening a document using the PATH_TO_PAPERS and the num from the outerloop, START is equal to 10000 I.E the first document
         with open(f"{PATH_TO_PAPERS}2101.{START+page_num}.html", encoding='UTF-8',) as file:
 
-
+                '''
+                This block creates a beatiful soup reader with the file opened in the line above this comment
+                Every attribute is searched for with only the find command instead of find_all because most documents only had 1 of the tags
+                If the searched returned None, no text will be extracted because it will throw an exception
+                There are also some regular expressions to remove tabs & newlines as they interfere with the tsv
+                I also turned the strings into a single line with the "".join(<string>.splitlines())
+                '''
                 soup = BeautifulSoup(file, "html.parser")
                 paper.title=(soup.find('title').text).strip()
                 authors=soup.find(class_="ltx_authors")
                 if authors!=None:
-                    paper.authors=re.sub(r"[\n\t]*", "",authors.text)
+                    paper.authors="".join(re.sub(r"[\n\t]*", "",authors.text).splitlines())
                 abstract = soup.find(class_="ltx_abstract")
                 if abstract!=None:
                     paper.abstract="".join(re.sub(r"[\n\t]*", "",abstract.text).splitlines())
